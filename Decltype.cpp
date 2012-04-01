7bdc2a235ba7e99fe41a5ce9175b2ef6c21413e9
dilshadsallo <dilshadsallo@gmail.com>
2012-04-01 18:41:23 +0100

    The decltype operator for querying the type of an expression.

--- Decltype.cpp
+++ Decltype.cpp
@@ -1,11 +1,13 @@
 // Dilshad Sallo, 18.3.2012
 // decltype(exp) will define variable j as the same as exp type.
+
 #include <iostream>
+#include <typeinfo>
 
 int main() {
-  int i = 7;
+  const int i = 7;
   const decltype(i) j = 8;
-  // which type?
-  std::cout << "The datatype of  " << j << " is integer." << std::endl;
+  
+  std::cout << "The datatype of  " << j << " is " << typeid(j).name() << "." << std::endl;
 
 }
