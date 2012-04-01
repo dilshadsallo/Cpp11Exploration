8d21c52948bfe4b52cc08e559833c9feaadf4731
dilshadsallo <dilshadsallo@gmail.com>
2012-04-01 18:40:43 +0100

    Using auto keyword to deduce the type of a variable.

--- Auto.cpp
+++ Auto.cpp
@@ -2,14 +2,13 @@
 // Using the auto keyword to deduce the type of a variable from its initializer expression.
 
 #include <iostream>
+#include <typeinfo>
 
 int main() {
   const auto i = 7;
   const auto j = 7.5;
 
-  // TODO: let C++ find out what is the type! What are the "reflection" means
-  // C++ (some news in C++11?)
-  std::cout << "The datatype of  " << i << " is integer." << std::endl;
-  std::cout << "The datatype of  " << j << " is double." << std::endl;
+  std::cout << "The datatype of  " << i << " is " << typeid(i).name() << "." << std::endl;
+  std::cout << "The datatype of  " << j << " is " << typeid(j).name() << "." << std::endl;
 
 }
