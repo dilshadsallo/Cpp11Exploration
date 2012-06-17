929ce50c26b54f9a549d065222c07c67c72700a6
unknown <Dilshad@Dilshad-PC.(none)>
2012-06-18 00:07:58 +0100

    using char16_t to store 16 bits.

---
+++ CoreLanguage/FunctionalityImprovements/Char16.cpp
@@ -0,0 +1,14 @@
+// Dilshad Sallo, 17.6.2012
+// Using char16_t to store character literal with 16 bits (UTF-16). 
+
+#include <iostream>
+#include <cassert>
+
+int main() {
+  const char16_t c1 = u'\u0061';
+  const char16_t c2 = u'a';
+
+  assert(sizeof(c1) == 2);
+  assert(sizeof(c2) == 2);
+
+}
