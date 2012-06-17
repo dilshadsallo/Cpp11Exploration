8d4128113caebd44cfbc9ba97dbcf8cf2a56ac59
unknown <Dilshad@Dilshad-PC.(none)>
2012-06-17 02:08:01 +0100

    Program to investigate long long integer.

---
+++ CoreLanguage/FunctionalityImprovements/Long.cpp
@@ -0,0 +1,14 @@
+// Dilshad Sallo, 18.3.2012
+//  Long long (int) takes at least 8 bytes.
+
+#include <iostream>
+#include <typeinfo>
+#include <cassert>
+
+int main() {
+  const long long ll = 1284800371338750000LL;
+  
+  std::cout << "the datatype of " << ll << " is " << typeid(ll).name() << "." << std::endl;
+  assert(sizeof(ll) >= 8);
+
+}
