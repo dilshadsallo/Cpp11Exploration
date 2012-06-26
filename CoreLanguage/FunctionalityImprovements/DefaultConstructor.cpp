be40cb98f41e8d44e08902437bd560b8a6d622b6
Dilshad <dilshad@ubuntu.(none)>
2012-06-26 01:51:12 +0100

    Defining explicit default constructor.

--- CoreLanguage/FunctionalityImprovements/DefaultConstructor.cpp
+++ CoreLanguage/FunctionalityImprovements/DefaultConstructor.cpp
@@ -1,22 +1,23 @@
 // Dilshad Sallo, 01.04.2012
-// Defining explicit default constructor and copy constructor.
+// Defining explicit default constructor.
 
 #include<iostream>
 
 class C {
+private:
   int i;
 public:
   C() = default; // Explicit default constructor
   C(int j);
 
-  C(const C& c) = default; // Explicit Default copy constructor
 };
 
-C::C(int j) {
- i = j;
+C::C(int j) : i(j) {
+  
 }
 
 int main() {
   C c1;
-  C c2(c1);
+  C c2(5);
 }
+
