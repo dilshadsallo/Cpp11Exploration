c503c9ee50b70748fd00839413ab637cd8b759d5
Dilshad <dilshad@ubuntu.(none)>
2012-06-27 00:43:17 +0100

    Using delete to prevent some methods.

---
+++ CoreLanguage/FunctionalityImprovements/DeleteNew.cpp
@@ -0,0 +1,17 @@
+// Dilshad Sallo  27.6.2012
+// Using delete keyword to prevent compiler 
+// from using operator new.
+
+#include <iostream>
+
+class C {
+public :
+  void *operator new(std::size_t) = delete; 
+  void *operator new[](std::size_t) = delete;
+};
+
+int main() {
+  //C *p = new C;
+  //C *p1 = new C[3];  
+  
+}
