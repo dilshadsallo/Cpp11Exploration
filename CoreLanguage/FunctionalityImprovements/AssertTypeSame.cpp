7604685ff08e6b2b5fb83d63222e7565e574f2fe
Dilshad <dilshad@ubuntu.(none)>
2012-06-30 21:06:32 +0100

    Using static_assert with type traits - relation types.

---
+++ CoreLanguage/FunctionalityImprovements/AssertTypeSame.cpp
@@ -0,0 +1,32 @@
+// Dilshad Sallo,30.06.2012
+// Using static_assert with type traits(is_same and is_base relations).
+
+#include <iostream>
+#include <type_traits>
+
+class A {};
+
+class A1: public A {    
+
+};
+
+class B {};
+
+class B1 : public B {
+    
+};
+
+template<class T1, class T2>
+void same(const T1& t1, const T2& t2) {
+  static_assert(std::is_same<T1, T2>::value, " T1 and T2 are not the same.");
+}
+
+template<class T1, class T2>
+void base(const T1& t1, const T2& t2) {
+  static_assert(std::is_base_of<T1, T2>::value, " T1 should be a base for T2.");
+}
+
+int main() {
+    same(A(), A() );
+    base(B(), B1());
+}
