6974e475585f105c0e911b8c41ab4d7f727c1ec6
Dilshad <dilshad@ubuntu.(none)>
2012-07-01 19:57:15 +0100

    Using static_assert with class.

---
+++ CoreLanguage/FunctionalityImprovements/AssertClass.cpp
@@ -0,0 +1,38 @@
+// Dilshad Sallo, 1.7.2012
+// Using static_assert to assert template parameter before using inside class.
+
+#include <iostream>
+#include <type_traits>
+
+template <class T>
+class C {
+private:
+  static_assert(std::is_integral<T>::value," type of T is not int.");
+  static_assert(std::is_unsigned<T>::value," type of T is signed.");
+    
+  T seconds;
+  T minutes;
+  T hours;
+public:
+  C(const T&, const T&, const T&);
+  void print();
+  
+};
+
+template <class T>
+C<T>::C(const T& hour, const T& min, const T& sec) {
+  seconds = sec;
+  minutes = min;
+  hours = hour;
+}
+
+template <class T>
+void C<T>::print() {
+  std::cout << hours << ":" << minutes << ":" << seconds;
+}
+
+int main() {
+
+  C <unsigned int> c(10,25,20);
+  c.print();
+}
