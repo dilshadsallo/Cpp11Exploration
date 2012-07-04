1c73b2c890823852d152559f12ba931b68b78541
dilshadsallo <dilshadsallo@gmail.com>
2012-07-04 23:31:27 +0100

    Using initializer-list with function.

---
+++ CoreLanguage/UsabilityEnhancements/InitializerListFunction.cpp
@@ -0,0 +1,23 @@
+// Dilshad Sallo, 4.7.2012
+// Using uniform initialization with initializer-list function.
+
+#include <iostream>
+#include <cassert>
+#include <initializer_list>
+
+double av(std::initializer_list<double> il) {
+  double total = 0;
+  int elements = il.size();
+  
+  for(auto p = il.begin(); p != il.end(); p++) {
+    total += *p;
+  }
+  
+  return(total/elements);
+}
+
+int main() {
+  double average = av({10,20,30,40,50,60,70});
+  assert(average == 40);
+  
+}
