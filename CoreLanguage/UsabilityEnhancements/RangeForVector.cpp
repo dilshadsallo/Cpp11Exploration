66490628da73c84014a3ec7819278d5ef195852c
Dilshad <dilshad@ubuntu.(none)>
2012-07-05 20:11:36 +0100

    Using range-based for with vector.

---
+++ CoreLanguage/UsabilityEnhancements/RangeForVector.cpp
@@ -0,0 +1,14 @@
+// Dilshad Sallo, 5.7.2012
+// Using range-based for statment with STL container (vector).
+
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<std::string> vec = {"Swansea", "University", "Computer", "Science"};
+    
+  for(const std::string i: vec) {
+    std::cout << i << std::endl;
+    
+  }
+}
