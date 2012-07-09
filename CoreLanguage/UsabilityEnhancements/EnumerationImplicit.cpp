1e0ee4ef0275116014a24ed40a07e0dc48ab6024
Dilshad <dilshad@ubuntu.(none)>
2012-07-09 19:17:22 +0100

    Using enumeration class prevent conversions.

---
+++ CoreLanguage/UsabilityEnhancements/EnumerationImplicit.cpp
@@ -0,0 +1,18 @@
+// Dilshad Sallo, 9.7.2012
+// Enumeration class prevent conversion to or from interger.
+
+#include <cassert>
+
+enum class E {
+  e1,
+  e2,
+  e3
+};
+
+int main() {
+  E e = E::e1;
+  assert(e == (E) 0);
+    
+  //assert(e == 0); // no E to int conversion.
+  //int i = E::e1;  // no E to int conversion.
+}
