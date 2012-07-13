e8a0324c8152b705471dda74728e9aa99ab7480d
dilshadsallo <dilshadsallo@gmail.com>
2012-07-14 00:58:59 +0100

    Using char16_t to concatenate string literals.

--- CoreLanguage/FunctionalityImprovements/String16.cpp
+++ CoreLanguage/FunctionalityImprovements/String16.cpp
@@ -1,7 +1,6 @@
 // Dilshad Sallo, 23.6.2012
 // Using char16_t  to concatenate string literals and store it with 16 bits. 
 
-#include <iostream>
 #include <cassert>
 
 int main() {
@@ -9,10 +8,10 @@ int main() {
   const char16_t *st2 = "a" u"b" "c";
   const char16_t *st3 = "a" "b" u"c";
   const char16_t *st4 = u"a" u"b" u"c";
-  
+
   const char16_t *st5 = u"abc";
   const char16_t s16[] = u"abcd";
- 
+
   assert (st1 == st5 && st2 == st5);
   assert (st3 == st5 && st4 == st5);
   assert (sizeof (s16) == 10);
