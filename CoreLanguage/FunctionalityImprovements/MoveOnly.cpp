0e10dc2a3dde8c0055b5d5a7b844fa8b5a6f9c6c
dilshadsallo <dilshadsallo@gmail.com>
2012-07-14 00:43:09 +0100

    Using delete to disable copies of functions.

--- CoreLanguage/FunctionalityImprovements/MoveOnly.cpp
+++ CoreLanguage/FunctionalityImprovements/MoveOnly.cpp
@@ -1,5 +1,5 @@
 // Dilshad Sallo, 27.6.2012
-// Using delete keyword to disable copies for some special members functions.
+// Using "delete" keyword to disable copies for some special members functions.
 
 #include <iostream>
 
@@ -8,23 +8,14 @@ public:
   T() = default;
   T(const T&) = delete;
   T& operator=(const T&) = delete;
-  T(T&&);
-  T& operator=(T&&);
+  T(T&&) {}
+  T& operator=(T&&) { return *this;}
 };
-
-T::T(T&&){
-    
-}
-
-T& T::operator=(T&&s){
-    
-    return *this;
-}
 
 int main() {
   T t1;
   T t2(std::move(t1));
   T t3;
   t3 = std::move(t2);
-     
+
 }
