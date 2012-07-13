4da5f06a67bdadb6ae64d1924ac50ec14232e37e
dilshadsallo <dilshadsallo@gmail.com>
2012-07-14 00:50:55 +0100

    Using raw string to represent symbols easily.

--- CoreLanguage/FunctionalityImprovements/RawString.cpp
+++ CoreLanguage/FunctionalityImprovements/RawString.cpp
@@ -2,11 +2,10 @@
 // Using raw string literal to deal easily with backslash,
 // quotation marks, comma and text. 
 
-#include <iostream>
 #include <cassert>
 #include <string.h>
 
 int main() {
   const char *s = R"('\\'|[:word:^.*()\\")";
-  assert (strcmp (s, "\'\\\\\'|[:word:^.*()\\\\\"") == 0);  
+  assert(strcmp (s, "\'\\\\\'|[:word:^.*()\\\\\"") == 0);  
 }
