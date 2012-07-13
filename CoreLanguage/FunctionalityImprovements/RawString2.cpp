185738e6599e9f90f5c812ef28d4d5fd34f936a9
dilshadsallo <dilshadsallo@gmail.com>
2012-07-14 00:56:24 +0100

    Storing the contents of raw string in 16 and 32 bits.

--- CoreLanguage/FunctionalityImprovements/RawString2.cpp
+++ CoreLanguage/FunctionalityImprovements/RawString2.cpp
@@ -1,13 +1,12 @@
 // Dilshad Sallo, 19.6.2012
 // Storing the contents of raw string in 16 and 32 bits. 
 
-#include <iostream>
 #include <cassert>
 
 int main() {
   const char16_t st[] = uR"(\"'() Store in 16-bits)";
   const char32_t st1[] = UR"(\"'() Store in 32-bits)";
-  
+
   assert (sizeof (st) == 46);
   assert (sizeof (st1) == 92);
 }
