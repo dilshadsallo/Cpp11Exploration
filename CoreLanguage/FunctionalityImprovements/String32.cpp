1ee57651b362d31f7ffab902a4aab164cefdf40c
dilshadsallo <dilshadsallo@gmail.com>
2012-07-14 01:00:14 +0100

    Using char32_t to concatenate string literals.

--- CoreLanguage/FunctionalityImprovements/String32.cpp
+++ CoreLanguage/FunctionalityImprovements/String32.cpp
@@ -1,7 +1,6 @@
 // Dilshad Sallo, 23.6.2012
 // Using char32_t  to concatenate string literals and store it with 32 bits. 
 
-#include <iostream>
 #include <cassert>
 
 int main() {
@@ -9,10 +8,10 @@ int main() {
   const char32_t *st2 = "a" U"b" "c";
   const char32_t *st3 = "a" "b" U"c";
   const char32_t *st4 = U"a" U"b" U"c";
-  
+
   const char32_t *st5 = U"abc";
   const char32_t s32[] = U"abcd";
- 
+
   assert (st1 == st5 && st2 == st5);
   assert (st3 == st5 && st4 == st5);
   assert (sizeof (s32) == 20);
