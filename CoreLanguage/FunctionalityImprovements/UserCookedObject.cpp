c371c5748c022a4227c6441101e13218fc56f620
dilshadsallo <dilshadsallo@gmail.com>
2012-07-14 01:25:21 +0100

    Using user-defined literals to return object.

--- CoreLanguage/FunctionalityImprovements/UserCookedObject.cpp
+++ CoreLanguage/FunctionalityImprovements/UserCookedObject.cpp
@@ -1,44 +1,30 @@
-// Dilshad Sallo, 
+// Dilshad Sallo, 29.6.2012
 // Using User-defined literals to return object instead of primitive type. 
 
-#include <iostream>
 #include <string>
 #include <string.h>
 #include <cassert>
 
 class C {
-private:
-  char str[20];
-  int  length;    
 public:
   C(const char *s, int len);
-  int getlen();
-  char* getstr();
+  int getlen() const { return length; }
+  char* getstr() { return str; }
+private:
+  char str[20];
+  int  length; 
 };  
- 
-C::C(const char *s, int len) {
-  
+
+C::C(const char *s, int len) {  
   strcpy(str,s); length = len; 
 }
 
-int C::getlen() {
-  return length;
-}
-
-char* C::getstr() {
-  return str;
-}
-
 C operator"" _i(const char* s, std::size_t len) { 
-
   return C(s,len);
 }
 
-int main() {
-    
+int main() {    
   C c1 = "abcd"_i;
   assert(strcmp(c1.getstr(),"abcd")==0);
   assert(c1.getlen() == 4);
-   
 }
-   
