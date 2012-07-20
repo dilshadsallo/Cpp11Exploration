193caf2527aa59ab1da40e604c225360f0f10d92
dilshadsallo <dilshadsallo@gmail.com>
2012-07-20 19:54:56 +0100

    Using "auto" keyword with templates.

---
+++ CoreLanguage/UsabilityEnhancements/Auto_Template.cpp
@@ -0,0 +1,16 @@
+// Dilshad Sallo, 
+// Using "auto" keyword with templates to deduce the type.
+
+#include <iostream>
+#include <typeinfo>
+
+template<class T1, class T2> void multiply(const T1& i, const T2& j) {
+  auto result = i * j;
+  std::cout << "the datatype of " << result << " is " << typeid(result).name() << " ." << std::endl;
+}
+
+int main() {
+  multiply(123.45,4);
+  multiply(555555,99999LL);
+  multiply('a', 1);
+}
