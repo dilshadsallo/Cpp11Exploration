af14646a7979ae78ba38605b7f76986dfcfef071
dilshadsallo <dilshadsallo@gmail.com>
2012-07-22 17:51:53 +0100

    Using alternative function syntax and decltype keyword witing template.

---
+++ CoreLanguage/UsabilityEnhancements/AlternativeFunction_Template.cpp
@@ -0,0 +1,25 @@
+// Dilshad Sallo, 22.7.2012
+// Using alternative function syntax and decltype keyword within template.
+
+#include <iostream>
+#include <typeinfo>
+
+template<class T1, class T2>
+auto add(const T1& x, const T2& y) -> decltype(x+y) {
+
+  return x+y;
+}
+
+int main() {
+  auto d = add(12,2.3);
+  auto j = add(3.4, 55LL);
+  auto l = add(3, 55LL);
+  auto i = add('a',2);
+  auto p = add("Hello world",6);
+
+  std::cout << "The datatype of  " << d << " is " << typeid(d).name() << "." << std::endl;
+  std::cout << "The datatype of  " << j << " is " << typeid(j).name() << "." << std::endl;
+  std::cout << "The datatype of  " << l << " is " << typeid(l).name() << "." << std::endl;
+  std::cout << "The datatype of  " << i << " is " << typeid(i).name() << "." << std::endl;
+  std::cout << "The datatype of  " << p << " is " << typeid(p).name() << "." << std::endl;
+}
