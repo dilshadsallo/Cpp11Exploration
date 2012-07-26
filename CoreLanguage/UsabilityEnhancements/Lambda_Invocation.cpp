f8f4f0c13d087f469f8fd1bf0f82f592bf037525
dilshadsallo <dilshadsallo@gmail.com>
2012-07-26 21:48:02 +0100

    Using lambda expression without parameters and return type.

---
+++ CoreLanguage/UsabilityEnhancements/Lambda_Invocation.cpp
@@ -0,0 +1,8 @@
+// Dilshad Sallo, 26.7.2012
+// Using lambda expression without parameters and return type to print string.
+
+#include <iostream>
+
+int main() {
+  []{std::cout<<"Hello World!." <<std::endl;}();
+}
