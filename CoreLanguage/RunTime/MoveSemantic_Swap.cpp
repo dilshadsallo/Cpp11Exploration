ff86c05464eb528dbd750e06486698081a744853
dilshadsallo <dilshadsallo@gmail.com>
2012-08-03 00:56:32 +0100

    Using move semantic to swap elements without duplicate copy.

---
+++ CoreLanguage/RunTime/MoveSemantic_Swap.cpp
@@ -0,0 +1,27 @@
+// Dilshad Sallo, 3.8.2012
+// Using move semantic to swap elements without duplicate copy.
+
+#include <iostream>
+#include <cassert>
+
+void f(int a[], const int size) {
+  for(int i = 0; i < size;){
+    for(int j = 0; j < size-1; j ++) {
+      if (a[i] < a[j]) {
+        int temp (std::move(a[i])); // one copy of a[i]
+        a[i] = std::move(a[j]);     // one copy of a[j]
+        a[j] = std::move(temp);    // one copy of temp
+      }
+    } i++;
+  }
+}
+
+int main() {
+  const int size = 10;
+  int arr[size] {6,2,0,8,3,9,1,4,7,5};
+  f(arr,size);
+
+  for(int i = 0; i<size; i++) {
+  assert(arr[i] == i);
+  }
+}
