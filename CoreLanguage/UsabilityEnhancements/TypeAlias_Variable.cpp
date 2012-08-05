67a0c71dfef9744e1a2ad86b1701dfc95aa05c69
dilshadsallo <dilshadsallo@gmail.com>
2012-08-06 00:52:16 +0100

    Using template alias with variables.

---
+++ CoreLanguage/UsabilityEnhancements/TypeAlias_Variable.cpp
@@ -0,0 +1,19 @@
+// Dilshad Sallo, 6.8.2012
+// Using template alias "using" keyword with varialbes
+
+#include <iostream>
+#include <typeinfo>
+
+int main() {
+  using myinteger = int;
+  using mydouble = double;
+  using mycharacter = char;
+
+  myinteger  x = 5;
+  mydouble y = 5.5;
+  mycharacter z = 'a';
+
+  std::cout << "the datatype of " << x << " is " << typeid(x).name() << std::endl;
+  std::cout << "the datatype of " << y << " is " << typeid(y).name() << std::endl;
+  std::cout << "the datatype of " << z << " is " <<typeid(z).name() << std::endl;
+}
