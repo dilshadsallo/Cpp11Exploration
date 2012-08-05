07a2de11ea14332d388321a72ccea161991a3fed
dilshadsallo <dilshadsallo@gmail.com>
2012-08-06 00:53:16 +0100

    Using template alias with a function pointer.

---
+++ CoreLanguage/UsabilityEnhancements/TypeAlias_FunctionPointer.cpp
@@ -0,0 +1,20 @@
+ // Dilshad Sallo, 6.8.2012
+// Using template alias "using" keyword with  a function pointer.
+
+#include <cassert>
+
+double multiply(int i, double d) {
+  return i * d;
+}
+
+int main() {
+  // using template alias
+  using pointer = double(*) (int, double);
+  pointer fp = multiply;
+  assert(fp(5,4.6) == 23);
+
+  //using typedef keyword
+  typedef double (*pointer1) (int, double);
+  pointer1 fp1 = multiply;
+  assert(fp1(2,4.5) == 9);
+}
