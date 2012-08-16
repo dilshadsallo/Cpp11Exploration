9f2fe5fa22fcc0ab65707d62a7d6fdf209934991
dilshadsallo <dilshadsallo@gmail.com>
2012-08-16 23:25:46 +0100

    Defining POD class in C++11 style.

---
+++ CoreLanguage/RunTime/POD_StandardLayoutClass.cpp
@@ -0,0 +1,30 @@
+// Dilshad Sallo,16.8.2012
+// Defining standard layout classes "C" and "A", but are not trivial classes and leading to be not POD type.
+
+#include <type_traits>
+
+class C {
+public:
+  C(int i, int j) : i(i), j(j) {}
+  C(const C& c) {}
+  C(C&& c) {}
+  C& operator=(const C& c) { return *this;}
+  C& operator=(C&& c) {return *this;}
+  ~C() {}
+private:
+  int i, j;
+};
+
+class A : C {
+  // C c; // it is not allowed in standard layout class.
+  // int k; // it is not allowed in standard layout class.
+};
+
+int main() {
+  static_assert( std::is_standard_layout<C>::value, "class C is not standard layout.");
+  static_assert( std::is_standard_layout<A>::value, "class A is not standard layout.");
+  // static_assert(std::is_trivial<C>::value, "class C is not trivial." );
+  // static_assert(std::is_trivial<A>::value, "class A is not trivial." );
+  // static_assert( std::is_pod<C>::value, "class C is not pod type.");
+  // static_assert( std::is_pod<A>::value, "class A is not pod type.");
+}
