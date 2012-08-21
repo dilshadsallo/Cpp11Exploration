9859c5d7c262e88dae8c3508d26dcb333a374672
dilshadsallo <dilshadsallo@gmail.com>
2012-08-21 19:51:59 +0100

    Using delegating constructor to call target constructor.

--- CoreLanguage/UsabilityEnhancements/DelegatingConstructor.cpp
+++ CoreLanguage/UsabilityEnhancements/DelegatingConstructor.cpp
@@ -1,15 +1,14 @@
 // Dilshad Sallo, 8.7.2012
 // Using delegating constructor to call target constructor.
 
-#include <iostream>
 #include <cassert>
 
 class C {
 private:
   int a, b;
 public:
-  C() : C(1,2) {}                  // delegating constructor 
-  C(int i): C(i,4) {}	           // delegating constructor
+  C() : C(1,2) {} // delegating constructor
+  C(int i): C(i,4) {} // delegating constructor
   C(int i, int j) : a(i), b(j) {} // target constructor
   void check();
 };
@@ -25,7 +24,7 @@ void C:: check () {
 int main() {
   C c1;
   C c2(3);
-  
+
   c1.check();
   c2.check();
 }
