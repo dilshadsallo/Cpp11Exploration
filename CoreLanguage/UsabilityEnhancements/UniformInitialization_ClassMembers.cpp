ce57f0fb8596495f476be60f493575779fd8aae6
dilshadsallo <dilshadsallo@gmail.com>
2012-08-21 19:55:32 +0100

    Using uniform initialisation to Initialise class members.

---
+++ CoreLanguage/UsabilityEnhancements/UniformInitialization_ClassMembers.cpp
@@ -0,0 +1,31 @@
+// Dilshad Sallo, 3.7.2012
+// Using uniform initialization (brace-initialization) to initialize the class members.
+
+#include <cassert>
+#include <string>
+
+class C {
+public:
+  void check();
+private:
+  int i {1};
+  int j {}; //default initialization
+  char c {'a'};
+  double d {2.3};
+  std::string s{"abc"};
+  int *p {};//default initialization
+};
+
+void C::check() {
+  assert( i == 1);
+  assert(j == 0);
+  assert( c == 'a');
+  assert( d == 2.3);
+  assert (s == "abc");
+  assert( p == nullptr);
+}
+
+int main() {
+  C c1;
+  c1.check();
+}
