f3edb9e7967fd22398dc10c1162201519a76b934
dilshadsallo <dilshadsallo@gmail.com>
2012-08-21 19:55:06 +0100

    Using uniform initialisation to Initialise dynamically allocated arrays.

---
+++ CoreLanguage/UsabilityEnhancements/UniformInitialization_DynamicArray.cpp
@@ -0,0 +1,9 @@
+// Dilshad Sallo, 3.7.2012 
+// Using uniform initialization (brace-initialization) to initialize dynamically allocated arrays.
+
+#include <cassert>
+
+int main() {
+  int *p = new int [2] {1,2};
+  assert((*p == 1) && (*++p == 2));
+}
