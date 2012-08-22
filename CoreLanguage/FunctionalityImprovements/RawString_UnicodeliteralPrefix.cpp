61abf348448cb7d109ae5e3bb6fa56a2d76e2e37
dilshadsallo <dilshadsallo@gmail.com>
2012-08-22 02:21:41 +0100

    Storing the contents of raw string in 8,16 and 32-bits form.

--- CoreLanguage/FunctionalityImprovements/RawString_UnicodeliteralPrefix.cpp
+++ CoreLanguage/FunctionalityImprovements/RawString_UnicodeliteralPrefix.cpp
@@ -1,13 +1,15 @@
 // Dilshad Sallo, 19.6.2012
-// Using raw string literals with unicode literal Prefixes ("u" and "U")
-// to store the content of raw string in 16 and 32 bits.
+// Using raw string literals with unicode literal Prefixes ("u8", u" and "U")
+// to store the content of raw string in 8,16 and 32 bits.
 
 #include <cassert>
 
 int main() {
-  const char16_t st[] = uR"(\"'() Store in 16-bits)";
-  const char32_t st1[] = UR"(\"'() Store in 32-bits)";
+  const char16_t st[] = u8R"(\"'() Store in 8-bits)";
+  const char16_t st1[] = uR"(\"'() Store in 16-bits)";
+  const char32_t st2[] = UR"(\"'() Store in 32-bits)";
 
-  assert (sizeof (st) == 46);
-  assert (sizeof (st1) == 92);
+  assert(sizeof(st)== 23);
+  assert(sizeof (st1) == 46);
+  assert(sizeof (st2) == 92);
 }
