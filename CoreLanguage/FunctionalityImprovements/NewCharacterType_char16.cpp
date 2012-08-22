0fb45c4d6e4819768d2912af57eb2e52b28c058b
dilshadsallo <dilshadsallo@gmail.com>
2012-08-22 02:18:59 +0100

    Using char16_t to store in 16-bits (UTF-16).

---
+++ CoreLanguage/FunctionalityImprovements/NewCharacterType_char16.cpp
@@ -0,0 +1,13 @@
+// Dilshad Sallo, 17.6.2012
+// Using char16_t to store character literal in 16-bits (UTF-16).
+
+#include <cassert>
+
+int main() {
+  const char16_t c1 = u'\u0061';//universal character name in 16-bit form
+  const char16_t c2 = u'a';
+
+  assert(c1 == c2);
+  assert(sizeof(c1) == 2);
+  assert(sizeof(c2) == 2);
+}
