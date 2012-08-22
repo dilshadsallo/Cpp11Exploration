2bc4e9d132e48d3b17769228fa0b8dff4e94c5a4
dilshadsallo <dilshadsallo@gmail.com>
2012-08-22 02:18:26 +0100

    Using char16_t to store in 32-bits form (UTF-32).

--- CoreLanguage/FunctionalityImprovements/NewCharacterType_char32.cpp
+++ CoreLanguage/FunctionalityImprovements/NewCharacterType_char32.cpp
@@ -1,15 +1,13 @@
 // Dilshad Sallo, 17.6.2012
-// Using char32_t to store character literal with 32 bits (UTF-32). 
+// Using char32_t to store character literal in 32-bits (UTF-32).
 
-#include <iostream>
 #include <cassert>
 
 int main() {
-  const char32_t c1 = U'\U00000061';
+  const char32_t c1 = U'\U00000061'; //universal character name in 32-bit form
   const char32_t c2 = U'a';
 
   assert(c1 == c2);
   assert(sizeof(c1) == 4);
   assert(sizeof(c2) == 4);
-
 }
