d7a58dd3293b3d708ea398215ab36040b34407b4
dilshadsallo <dilshadsallo@gmail.com>
2012-08-26 17:55:30 +0100

    Implementing move assignment operator by using rvalue reference.

---
+++ CoreLanguage/RunTime/MoveSemantic_AssignmentOperator.cpp
@@ -0,0 +1,49 @@
+// Dilshad Sallo, 26.8.2012
+// Implementing move assignment operator by using rvalue references.
+
+#include <iostream>
+
+class C {
+public:
+  C() = default;
+  C(int i) : i(i), p(new int [i]) { std::cout << "Normal constructor." << std::endl;}
+  C(const C&) = default;
+  C& operator=(const C&) { std::cout << "Copy assignment Operator." << std::endl; return *this; }
+  C& operator=(C&& c);
+private:
+  int i;
+  int* p;
+};
+
+C& C::operator=(C&& c) {
+  std::cout << "Move assignment operator." << std::endl;
+  //check for self-assignment
+  if(this == &c) {
+    return *this;
+  } else {
+  // Free the existing resource.
+   delete [] p;
+
+  // shallow copy of data
+  i = c.i;
+  p = c.p;
+
+  //Reset the source object
+  c.i = 0;
+  c.p = 0;//nullptr;
+  }
+  
+  return *this; 
+}
+
+C createObject() {
+  return C(1);
+}
+int main() {
+ C c1(5);
+ c1 = createObject(); // move assignment operator because return object is temporary 
+  C c2(3);
+  c2 = (std::move(c1)); // call move assignment operator
+  C c3;
+  c3 = c1;// copy assignment operator
+}
