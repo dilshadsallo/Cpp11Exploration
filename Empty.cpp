c702304b6c5a201dd0b88f6ec206671e2485e185
dilshadsallo <dilshadsallo@gmail.com>
2012-08-26 23:26:23 +0100

    Update document

--- Empty.cpp
+++
@@ -1,5 +0,0 @@
-// Oliver Kullmann, 29.2.2012 (Swansea)
-// This is the real first C++ program!
-
-int main() {}
- 
--- Return.cpp
+++
@@ -1,5 +0,0 @@
-// Oliver Kullmann, 29.2.2012 (Swansea)
-
-int main() {
-  return 1;
-}
