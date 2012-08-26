c485b3889f58e4b04509d1fd68af1a4b72f1f2b8
dilshadsallo <dilshadsallo@gmail.com>
2012-08-26 22:33:54 +0100

    Using "override" keyword to mark functions explicitly as override.

---
+++ CoreLanguage/UsabilityEnhancements/OverrideKeyword.cpp
@@ -0,0 +1,42 @@
+// Dilshad Sallo, 02.04.2012
+// Using "override" keyword to mark functions explicitly as override.
+
+#include <cassert>
+
+class Base {
+public:
+  Base() : x(1) {}
+  virtual int f(int i) { return i+x;}
+private:
+  const int x;
+};
+
+class Derived1 : Base {
+public:
+  Derived1() : y(2) {}
+  virtual int f(int i) override { return i + y;} // explicit override
+private:
+  const int y;
+};
+
+class Derived2 : Base {
+public:
+  Derived2() : z(3) {}
+  virtual int f(int i) override { return i+z; } // explicit override
+private:
+  const int z;
+};
+
+int main() {
+  Base b;
+  Derived1 d1;
+  Derived2 d2;
+
+  const int bvalue = b.f(2);
+  const int dvalue1 = d1.f(2);
+  const int dvalue2 = d2.f(2);
+
+  assert( bvalue == 3);
+  assert( dvalue1 == 4);
+  assert( dvalue2 == 5);
+}
