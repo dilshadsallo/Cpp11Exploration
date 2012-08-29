20b42c8b65fd164139fb098eeb7b963be6e49ce4
dilshadsallo <dilshadsallo@gmail.com>
2012-08-29 13:03:22 +0100

    Using uniform initialization to initalize vector.

--- CoreLanguage/UsabilityEnhancements/InitializerList_Vector.cpp
+++ CoreLanguage/UsabilityEnhancements/InitializerList_Vector.cpp
@@ -1,22 +1,18 @@
 // Dilshad Sallo, 4.7.2012
-// Using uniform initialization to initialize vector.
+// Using uniform initialization to initialize vector implicitly via template class "initializer_list".
 
-#include <iostream>
 #include <string>
 #include <cassert>
 #include <vector>
 
 int main() {
-  
   std::vector<std::string> vec;
   vec.push_back("Computer");
   vec.push_back("Science");
   vec.push_back("Department");
 
   std::vector<std::string> vu = {"Computer", "Science", "Department"};
-  
   assert(vu.at(0) == vec.at(0));
   assert(vu.at(1) == vec.at(1));
   assert(vu.at(2) == vec.at(2));
-
 }
