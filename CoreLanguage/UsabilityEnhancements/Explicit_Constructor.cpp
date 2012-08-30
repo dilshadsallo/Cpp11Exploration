c74a36b6cf419b20654e037c2a90b0a4913f430d
dilshadsallo <dilshadsallo@gmail.com>
2012-08-30 11:06:12 +0100

    Using explicit keyword with constructor to prevent implcit converstion.

--- CoreLanguage/UsabilityEnhancements/Explicit_Constructor.cpp
+++ CoreLanguage/UsabilityEnhancements/Explicit_Constructor.cpp
@@ -1,33 +1,41 @@
 // Dilshad Sallo, 29.7.2012
-// Using "explicit" keyword with constructors to prevent implicit converstion.
+// Using "explicit" keyword with constructors to prevent implicit conversion.
 
 #include <cassert>
 #include <string>
+
 class A{
 public:
   A() = default;
-  explicit A(const int i) : x(i) {} // prevent implicit converstion
-  A(const std::string& str): s(str) {}
-  const A operator+ (const A& a) const;
-  const std::string getstring() const { return s;}
+  A(const int i) : x(i) {}
+  int get_value() const { return x;}
+  const A operator+ (const A& a) const {
+    A temp;
+    temp.x = x + a.x;
+    return temp;
+  }
 private:
   int x;
-  std::string s;
 };
 
-const A A::operator+(const A& a) const {
-  A temp;
-  temp.x = x + a.x;
-  temp.s = s + a.s;
-  return temp;
+class B {
+public:
+    B() = default;
+    explicit B(int i) : y(i) {} // prevent implicit conversion
+ const B operator+ (const B& b) const {
+  B temp;
+ temp.y = y + b.y;
+ return temp;
 }
-
+private:
+ int y;
+};
 int main() {
-  A a1("Computer ");
-  std::string s = "Science";
-  A a2 = a1 + s;
-  assert(a2.getstring() == "Computer Science");
+  A a1 = 1; // implicit conversion
+  assert(a1.get_value() == 1);
+  A a2 = a1 + 2; //implicit conversion
+  assert(a2.get_value() == 3);
 
-  A a3(1);
-  //A a4 = a3 + 2; // Error: no match for ‘operator+’ in ‘a3 + 2’
+  //B b = 1; // Error : implicit conversion is not allowed
+  //B b2 = b + 3; // Error : implicit conversion is not allowed
 }
