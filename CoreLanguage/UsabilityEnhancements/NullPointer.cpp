b993f86092605c34dfe826df57a272da207ddc24
dilshadsallo <dilshadsallo@gmail.com>
2012-09-28 14:35:38 +0100

    Using null pointer constant "nullptr" as a parameter to a function.

--- CoreLanguage/UsabilityEnhancements/NullPointer.cpp
+++ CoreLanguage/UsabilityEnhancements/NullPointer.cpp
@@ -1,5 +1,8 @@
-// Dilshad Sallo, 6.8.2012
-// Using null pointer constant "nullptr" as a parameter to a function.
+/**
+ * \author Dilshad Sallo
+ * \date 2012/8/6
+ * \brief Using null pointer constant "nullptr" as a parameter to a function.
+ */
 
 #include <cassert>
 
