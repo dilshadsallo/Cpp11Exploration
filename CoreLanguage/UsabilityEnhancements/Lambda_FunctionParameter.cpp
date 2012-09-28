19d97a9b3b2b10bb15f4ae93ce45413fd7dceaea
dilshadsallo <dilshadsallo@gmail.com>
2012-09-28 14:23:12 +0100

    Using lambda expression as parameter.

--- CoreLanguage/UsabilityEnhancements/Lambda_FunctionParameter.cpp
+++ CoreLanguage/UsabilityEnhancements/Lambda_FunctionParameter.cpp
@@ -1,5 +1,8 @@
-// Dilshad Sallo, 26.7.2012
-// Using lambda expression as parameter with std::function.
+/**
+ * \author Dilshad Sallo
+ * \date 2012/7/26
+ * \brief Using lambda expression as parameter with std::function.
+ */
 
 #include <functional>
 #include <initializer_list>
